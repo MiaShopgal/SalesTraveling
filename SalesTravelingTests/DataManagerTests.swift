5ee4d40ae53846c0a6d4ce3f9d4d8021640b7b2d
₥ia <mia.shopgal@gmail.com>
2024-01-23 17:06:46 +0800

    add test class

---
+++ SalesTravelingTests/DataManagerTests.swift
@@ -0,0 +1,20 @@
+//
+//  DataManagerTests.swift
+//  SalesTravelingTests
+//
+//  Created by Mia Yu on 2024/1/23.
+//  Copyright © 2024 Hanyu. All rights reserved.
+//
+
+import XCTest
+@testable import Shoto
+import MapKit
+
+class DataManagerTests: XCTestCase {
+
+    func test(){
+        
+        
+       
+    }
+}
